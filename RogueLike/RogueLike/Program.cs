16ee49296aeb65c6cf4a0f92be3fe1f14d2b280b
Colten Smith <coltensmith220119@gmail.com>
2023-06-28 14:10:57 -0400

    LETS TRY THIS CRAP AGAIN SHALL WE

--- RogueLike/RogueLike/Program.cs
+++ RogueLike/RogueLike/Program.cs
@@ -1,36 +1,15 @@
-﻿using RogueLike.Classes;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Reflection.Metadata.Ecma335;
 
 namespace RogueLike
 {
-    //TODO redo program logic by starting with tests
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO Refactor entire program
 
-            //Console.WriteLine("   │·│      ");
-            //Console.WriteLine("┌──┘·└─────┐");
-            //Console.WriteLine("│··········└────┐");
-            //Console.WriteLine("│···@······%····│");
-            //Console.WriteLine("│······?···┌┐···│");
-            //Console.WriteLine("└──────────┘└───┘");
-            LevelBuilder lb = new LevelBuilder();
-            lb.TopLeftCornerWall();
-            lb.HorizontalWall(8);
-            lb.TopRightCornerWall();
-            lb.VerticalWall(8);
-            lb.BottomRightCornerWall();
-            lb.HorizontalWall(8, true);
-            lb.BottomLeftCornerWall();
-            lb.VerticalWall(8, true);
-            lb.AddPlayerCharacter(5, 6);
-            byte[] SBP = { 1, 1, 1, 1, 3, 3, 3, 3 };
-            lb.AddSawBlade(6, 3, SBP);
-            lb.AddLockedDoor(8, 6);
-            Chart chart = new Chart(lb.GetLevel(), 10, 10);
-            UI.DisplayChart(chart);
         }
     }
 }
